63c313b6f55e4e0226e6a4ce59bdaee8091b50fb
DiskKun <luca5h1905@gmail.com>
2024-09-27 13:13:32 -0400

    Week 4 Tasks 1-3

---
+++ A1-SpaceShooter-V2/Assets/Scripts/Controllers/Moon.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    public Transform planetTransform;
+    public float orbitRadius;
+    public float orbitSpeed;
+
+    float orbitAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OrbitalMotion(orbitRadius, orbitSpeed, planetTransform);
+    }
+
+    void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        orbitAngle += Time.deltaTime * speed;
+        float rad = orbitAngle * Mathf.Deg2Rad;
+        transform.position = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad)) * radius + target.position;
+    }
+}
--- A1-SpaceShooter-V2/Assets/Scripts/Controllers/Player.cs
+++ A1-SpaceShooter-V2/Assets/Scripts/Controllers/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public GameObject powerupPrefab;
+    public int radarPoints;
+    public float radarRadius;
 
     Vector3 velocity;
     float acceleration;
@@ -16,6 +19,8 @@ public class Player : MonoBehaviour
 
     float deceleration;
     float decelerationTime = 0.4f;
+
+    List<float> circleAngles = new List<float>();
 
 
     private void Start()
@@ -27,20 +32,52 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        EnemyRadar(radarRadius, radarPoints);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnPowerups(2, 9);
+        }
+
     }
+
+    void SpawnPowerups(float radius, int numberOfPowerups)
+    {
+        float angleIncrement = 360 / numberOfPowerups;
+        for (int i = 0; i < numberOfPowerups; i++)
+        {
+            circleAngles.Add(angleIncrement * i * Mathf.Deg2Rad);
+        }
+        for (int i = 0; i < circleAngles.Count; i++)
+        {
+            Vector3 pos1 = new Vector2(Mathf.Cos(circleAngles[i]), Mathf.Sin(circleAngles[i])) * radius;
+            Instantiate(powerupPrefab, pos1 + transform.position, Quaternion.identity);
+        }
+        circleAngles.Clear();
+    }
+
+    void EnemyRadar(float radius, int circlePoints)
+    {
+        Color radarColor = (Mathf.Abs((enemyTransform.position - transform.position).magnitude) < radius) ? Color.red : Color.green;
+        float angleIncrement = 360 / circlePoints;
+        for (int i = 0; i < circlePoints; i++)
+        {
+            circleAngles.Add(angleIncrement * i * Mathf.Deg2Rad);
+        }
+        for (int i = 0; i < circleAngles.Count; i++)
+        {
+            int pos2Index = (i + 1) % circleAngles.Count;
+            Vector3 pos1 = new Vector2(Mathf.Cos(circleAngles[i]), Mathf.Sin(circleAngles[i])) * radius;
+            Vector3 pos2 = new Vector2(Mathf.Cos(circleAngles[pos2Index]), Mathf.Sin(circleAngles[pos2Index])) * radius;
+            Debug.DrawLine(pos1 + transform.position, pos2 + transform.position, radarColor);
+        }
+        circleAngles.Clear();
+    } 
 
     void PlayerMovement()
     {
-        if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
-        {
-            velocity += velocity.normalized * deceleration * Time.deltaTime;
-            if (velocity.magnitude < 0.01f)
-            {
-                velocity = Vector3.zero;
-            }
-        }
-        else
-        {
+        
+ 
             Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             velocity += direction.normalized * acceleration * Time.deltaTime;
             if (velocity.magnitude > maxSpeed)
@@ -50,8 +87,23 @@ public class Player : MonoBehaviour
             {
                 velocity = velocity.normalized * -maxSpeed;
             }
-        }
         
+        if (Input.GetAxisRaw("Vertical") == 0)
+        {
+            velocity.y += velocity.normalized.y * deceleration * Time.deltaTime;
+            //if (velocity.y < 0.01f)
+            //{
+            //    velocity.y = 0;
+            //}
+        }
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            velocity.x += velocity.normalized.x * deceleration * Time.deltaTime;
+            //if (velocity.x < 0.01f)
+            //{
+            //    velocity.x = 0;
+            //}
+        }
         transform.position += velocity * Time.deltaTime;
     }
 }
